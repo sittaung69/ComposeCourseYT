be60135a1e1ec236600a1bb0e9e6f65e3bacc1bf
Sitt Aung <sittaung2021@gmail.com>
2021-07-05 16:39:53 +0630

    Side Effects & Effect Handlers

--- app/src/main/java/com/sitt/composecourseyt/MainActivity.kt
+++ app/src/main/java/com/sitt/composecourseyt/MainActivity.kt
@@ -2,37 +2,39 @@ package com.sitt.composecourseyt
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.OnBackPressedCallback
+import androidx.activity.OnBackPressedDispatcher
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
 import androidx.compose.ui.tooling.preview.Preview
 import com.sitt.composecourseyt.ui.theme.ComposeCourseYTTheme
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeCourseYTTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+            val scaffoldState = rememberScaffoldState()
+            val scope = rememberCoroutineScope()
+            Scaffold(scaffoldState = scaffoldState) {
+                var counter = produceState(initialValue = 0) {
+                    delay(3000L)
+                    value = 4
+                }
+                if (counter.value % 5 == 0 && counter.value > 0) {
+                    LaunchedEffect(key1 = scaffoldState.snackbarHostState) {
+                        scaffoldState.snackbarHostState.showSnackbar("Hello")
+                    }
+                }
+                Button(onClick = { }) {
+                    Text("Click Me: ${counter.value}")
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeCourseYTTheme {
-        Greeting("Android")
     }
 }
